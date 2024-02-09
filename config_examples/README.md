221c994d3c92ea80d1efdf08b9d42b390f7867ce
Joao Grassi <joao@joaograssi.com>
2024-02-09 16:32:25 +0100

    Add e2e test for tail sampling config example (#129)
    * Add initial test
    
    * cleanup
    
    * Add spaces back
    
    * Add readme
    
    * Grammar
    
    * Pr suggestions
    
    * fix name
    
    * PR suggestions
    
    * PR suggestions

---
+++ config_examples/README.md
@@ -0,0 +1,51 @@
+# Collector example configurations
+
+Here you can find a collection of example Collector configurations.
+
+## Samples
+
+- [Drop](drop.yaml)
+- [Effective](effective.yaml.yaml)
+- [Health check](healthcheck.yaml)
+- [Jaeger Receiver](jaeger.yaml)
+- [Pipeline](pipeline.yaml)
+- [Tail sampling](tail_sampling.yaml)
+
+## Sending data to Dynatrace
+
+In addition to the `debug` exporter, some samples are also configured with the `otlphttp` exporter
+so you can also see the data in your Dynatrace environment.
+
+Before running the samples, make sure you have the following environment variables set:
+
+- `DT_OTLP_ENDPOINT`: The OTLP HTTP endpoint of your Dynatrace environment.
+  - Follow the guide: [Export to ActiveGate](https://docs.dynatrace.com/docs/shortlink/otel-getstarted-otlpexport#export-to-activegate)
+    to see how to get the correct API URL for your environment
+- `API_TOKEN`: The Dynatrace API access token. Follow the guide on [Authentication](https://docs.dynatrace.com/docs/shortlink/otel-getstarted-otlpexport#authentication-export-to-activegate) to see the scopes required for ingesting OTLP data.
+
+## Trying out the configuration examples
+
+You can try each configuration example by simply passing the file to the collector when starting up:
+
+```shell
+docker run --rm \
+  --name dt-otelcol \
+  --env DT_OTLP_ENDPOINT=$DT_OTLP_ENDPOINT \
+  --env API_TOKEN=$API_TOKEN \
+  -p 4317:4317 \
+  -v $(pwd)/config_examples/tail_sampling.yaml:/collector.yaml \
+  ghcr.io/dynatrace/dynatrace-otel-collector/dynatrace-otel-collector:latest \
+  --config collector.yaml
+```
+
+Or try it out from the GitHub repo if you don't have it cloned:
+
+```shell
+docker run --rm \
+  --name dt-otelcol \
+  --env DT_OTLP_ENDPOINT=$DT_OTLP_ENDPOINT \
+  --env API_TOKEN=$API_TOKEN \
+  -p 4317:4317 \
+  ghcr.io/dynatrace/dynatrace-otel-collector/dynatrace-otel-collector:latest \
+  --config https://raw.githubusercontent.com/Dynatrace/dynatrace-otel-collector/main/config_examples/tail_sampling.yaml
+```
---
+++ testbed/integration/validation.go
@@ -0,0 +1,73 @@
+package integration
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/testbed/testbed"
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+)
+
+var _ testbed.TestCaseValidator = &SampleConfigsValidator{}
+
+type SampleConfigsValidator struct {
+	expectedTraces ptrace.Traces
+	t              *testing.T
+}
+
+func NewSampleConfigsValidator(t *testing.T, expectedTraces ptrace.Traces) *SampleConfigsValidator {
+	return &SampleConfigsValidator{
+		expectedTraces: expectedTraces,
+		t:              t,
+	}
+}
+
+func (v *SampleConfigsValidator) Validate(tc *testbed.TestCase) {
+	actualSpans := tc.MockBackend.DataItemsReceived()
+
+	assert.EqualValues(v.t, v.expectedTraces.SpanCount(), actualSpans, "Received and expected number of spans do not match.")
+	assertExpectedSpansAreInReceived(v.t, []ptrace.Traces{v.expectedTraces}, tc.MockBackend.ReceivedTraces)
+}
+
+func (v *SampleConfigsValidator) RecordResults(tc *testbed.TestCase) {
+}
+
+func assertExpectedSpansAreInReceived(t *testing.T, expected, actual []ptrace.Traces) {
+	spansMap := make(map[string]ptrace.Span)
+	populateSpansMap(spansMap, expected)
+
+	for _, td := range actual {
+		rss := td.ResourceSpans()
+		for i := 0; i < rss.Len(); i++ {
+			ss := rss.At(i).ScopeSpans()
+			for j := 0; j < ss.Len(); j++ {
+				spans := ss.At(j).Spans()
+				for k := 0; k < spans.Len(); k++ {
+					recdSpan := spans.At(k)
+					assert.Contains(t,
+						spansMap,
+						traceIDAndSpanIDToString(recdSpan.TraceID(), recdSpan.SpanID()),
+						fmt.Sprintf("Span with ID: %q not found among expected spans", recdSpan.SpanID()))
+				}
+			}
+		}
+	}
+}
+
+func populateSpansMap(spansMap map[string]ptrace.Span, tds []ptrace.Traces) {
+	for _, td := range tds {
+		rss := td.ResourceSpans()
+		for i := 0; i < rss.Len(); i++ {
+			ilss := rss.At(i).ScopeSpans()
+			for j := 0; j < ilss.Len(); j++ {
+				spans := ilss.At(j).Spans()
+				for k := 0; k < spans.Len(); k++ {
+					span := spans.At(k)
+					key := traceIDAndSpanIDToString(span.TraceID(), span.SpanID())
+					spansMap[key] = span
+				}
+			}
+		}
+	}
+}

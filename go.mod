17a4f04d75256b2c2e6486817c1ac488f0637efd
Joao Grassi <5938087+joaopgrassi@users.noreply.github.com>
2024-04-24 17:15:03 +0200

    WIP

--- go.mod
+++ go.mod
@@ -5,22 +5,34 @@ go 1.21.0
 toolchain go1.22.2
 
 require (
+	github.com/Dynatrace/dynatrace-otel-collector/internal/k8stest v0.0.0
+	github.com/google/uuid v1.6.0
 	github.com/open-telemetry/opentelemetry-collector-contrib/testbed v0.99.0
 	github.com/stretchr/testify v1.9.0
 	go.opentelemetry.io/collector/component v0.99.0
+	go.opentelemetry.io/collector/consumer v0.99.0
 	go.opentelemetry.io/collector/pdata v1.6.0
+	go.opentelemetry.io/collector/receiver v0.99.0
+	go.opentelemetry.io/collector/receiver/otlpreceiver v0.99.0
 	go.opentelemetry.io/collector/semconv v0.99.0
+	go.uber.org/multierr v1.11.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/DataDog/datadog-agent/pkg/trace/exportable v0.0.0-20201016145401-4646cf596b02 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/apache/thrift v0.20.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/census-instrumentation/opencensus-proto v0.4.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/distribution/reference v0.5.0 // indirect
+	github.com/docker/docker v25.0.5+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/expr-lang/expr v1.16.5 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fluent/fluent-logger-golang v1.9.0 // indirect
@@ -28,6 +40,9 @@ require (
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-openapi/jsonpointer v0.20.2 // indirect
+	github.com/go-openapi/jsonreference v0.20.4 // indirect
+	github.com/go-openapi/swag v0.22.9 // indirect
 	github.com/go-viper/mapstructure/v2 v2.0.0-alpha.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gogo/googleapis v1.4.1 // indirect
@@ -35,14 +50,16 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/uuid v1.6.0 // indirect
+	github.com/google/gnostic-models v0.6.8 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1 // indirect
-	github.com/haimrubinstein/go-syslog/v3 v3.0.0 // indirect
 	github.com/hashicorp/go-version v1.6.0 // indirect
+	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/influxdata/go-syslog/v3 v3.0.1-0.20230911200830-875f5bc594a4 // indirect
 	github.com/jaegertracing/jaeger v1.56.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.8 // indirect
@@ -50,11 +67,13 @@ require (
 	github.com/knadh/koanf/v2 v2.1.1 // indirect
 	github.com/leodido/ragel-machinery v0.0.0-20181214104525-299bdde78165 // indirect
 	github.com/lufia/plan9stats v0.0.0-20220913051719-115f729f3c8c // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mostynb/go-grpc-compression v1.2.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/open-telemetry/opentelemetry-collector-contrib/exporter/carbonexporter v0.99.0 // indirect
 	github.com/open-telemetry/opentelemetry-collector-contrib/exporter/opencensusexporter v0.99.0 // indirect
 	github.com/open-telemetry/opentelemetry-collector-contrib/exporter/prometheusexporter v0.99.0 // indirect
@@ -82,8 +101,11 @@ require (
 	github.com/open-telemetry/opentelemetry-collector-contrib/receiver/syslogreceiver v0.99.0 // indirect
 	github.com/open-telemetry/opentelemetry-collector-contrib/receiver/zipkinreceiver v0.99.0 // indirect
 	github.com/open-telemetry/opentelemetry-collector-contrib/testbed/mockdatasenders/mockdatadogagentexporter v0.99.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/openzipkin/zipkin-go v0.4.2 // indirect
 	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/power-devops/perfstat v0.0.0-20220216144756-c35f1ee13d7c // indirect
 	github.com/prometheus/client_golang v1.19.0 // indirect
@@ -123,7 +145,6 @@ require (
 	go.opentelemetry.io/collector/confmap/provider/httpsprovider v0.99.0 // indirect
 	go.opentelemetry.io/collector/confmap/provider/yamlprovider v0.99.0 // indirect
 	go.opentelemetry.io/collector/connector v0.99.0 // indirect
-	go.opentelemetry.io/collector/consumer v0.99.0 // indirect
 	go.opentelemetry.io/collector/exporter v0.99.0 // indirect
 	go.opentelemetry.io/collector/exporter/debugexporter v0.99.0 // indirect
 	go.opentelemetry.io/collector/exporter/otlpexporter v0.99.0 // indirect
@@ -137,8 +158,6 @@ require (
 	go.opentelemetry.io/collector/processor v0.99.0 // indirect
 	go.opentelemetry.io/collector/processor/batchprocessor v0.99.0 // indirect
 	go.opentelemetry.io/collector/processor/memorylimiterprocessor v0.99.0 // indirect
-	go.opentelemetry.io/collector/receiver v0.99.0 // indirect
-	go.opentelemetry.io/collector/receiver/otlpreceiver v0.99.0 // indirect
 	go.opentelemetry.io/collector/service v0.99.0 // indirect
 	go.opentelemetry.io/contrib/config v0.5.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.50.0 // indirect
@@ -160,17 +179,35 @@ require (
 	go.opentelemetry.io/otel/sdk/metric v1.25.0 // indirect
 	go.opentelemetry.io/otel/trace v1.25.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.2.0 // indirect
-	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
+	golang.org/x/mod v0.16.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/oauth2 v0.18.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/term v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/tools v0.19.0 // indirect
 	gonum.org/v1/gonum v0.15.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240314234333-6e1732d8331c // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240401170217-c3f982113cda // indirect
 	google.golang.org/grpc v1.63.2 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/api v0.29.3 // indirect
+	k8s.io/apimachinery v0.29.3 // indirect
+	k8s.io/client-go v0.29.3 // indirect
+	k8s.io/klog/v2 v2.120.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20231010175941-2dd684a91f00 // indirect
+	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
 )
 
 replace cloud.google.com/go => cloud.google.com/go v0.112.2
+
+replace github.com/Dynatrace/dynatrace-otel-collector/internal/k8stest v0.0.0 => ./internal/k8stest
---
+++ testbed/integration/k8senrichment/e2e_test.go
@@ -0,0 +1,405 @@
+package k8senrichment
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/component/componenttest"
+	"go.opentelemetry.io/collector/consumer/consumertest"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/receiver/otlpreceiver"
+	"go.opentelemetry.io/collector/receiver/receivertest"
+	"go.uber.org/multierr"
+
+	"github.com/Dynatrace/dynatrace-otel-collector/internal/k8stest"
+)
+
+const (
+	equal = iota
+	regex
+	exist
+	testKubeConfig = "/home/grassi/.kube/config"
+	uidRe          = "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}"
+	startTimeRe    = "^\\\\d{4}-\\\\d{2}-\\\\d{2}T\\\\d{2}%3A\\\\d{2}%3A\\\\d{2}(?:%2E\\\\d+)?[A-Z]?(?:[+.-](?:08%3A\\\\d{2}|\\\\d{2}[A-Z]))?$"
+)
+
+type expectedValue struct {
+	mode  int
+	value string
+}
+
+func newExpectedValue(mode int, value string) *expectedValue {
+	return &expectedValue{
+		mode:  mode,
+		value: value,
+	}
+}
+
+// TestE2E_ClusterRBAC tests the k8s attributes processor in a k8s cluster with the collector's service account having
+// cluster-wide permissions to list/watch namespaces, nodes, pods and replicasets. The config in the test does not
+// set filter::namespace.
+// The test requires a prebuilt otelcontribcol image uploaded to a kind k8s cluster defined in
+// `/tmp/kube-config-otelcol-e2e-testing`. Run the following command prior to running the test locally:
+//
+//	kind create cluster --kubeconfig=/tmp/kube-config-otelcol-e2e-testing
+//	make docker-otelcontribcol
+//	KUBECONFIG=/tmp/kube-config-otelcol-e2e-testing kind load docker-image otelcontribcol:latest
+func TestE2E_ClusterRBAC(t *testing.T) {
+	testDir := filepath.Join("testdata")
+
+	k8sClient, err := k8stest.NewK8sClient(testKubeConfig)
+	require.NoError(t, err)
+
+	// Create the namespace specific for the test
+	nsFile := filepath.Join(testDir, "namespace.yaml")
+	buf, err := os.ReadFile(nsFile)
+	require.NoErrorf(t, err, "failed to read namespace object file %s", nsFile)
+	nsObj, err := k8stest.CreateObject(k8sClient, buf)
+	require.NoErrorf(t, err, "failed to create k8s namespace from file %s", nsFile)
+
+	testNs := nsObj.GetName()
+	defer func() {
+		require.NoErrorf(t, k8stest.DeleteObject(k8sClient, nsObj), "failed to delete namespace %s", testNs)
+	}()
+
+	metricsConsumer := new(consumertest.MetricsSink)
+	tracesConsumer := new(consumertest.TracesSink)
+	logsConsumer := new(consumertest.LogsSink)
+	shutdownSinks := startUpSinks(t, metricsConsumer, tracesConsumer, logsConsumer)
+	defer shutdownSinks()
+
+	testID := uuid.NewString()[:8]
+	collectorObjs := k8stest.CreateCollectorObjects(t, k8sClient, testID, filepath.Join(testDir, "collector"))
+	createTeleOpts := &k8stest.TelemetrygenCreateOpts{
+		ManifestsDir: filepath.Join(testDir, "telemetrygen"),
+		TestID:       testID,
+		OtlpEndpoint: fmt.Sprintf("otelcol-%s.%s:4317", testID, testNs),
+		DataTypes:    []string{"metrics", "logs", "traces"},
+	}
+	telemetryGenObjs, telemetryGenObjInfos := k8stest.CreateTelemetryGenObjects(t, k8sClient, createTeleOpts)
+	defer func() {
+		for _, obj := range append(collectorObjs, telemetryGenObjs...) {
+			require.NoErrorf(t, k8stest.DeleteObject(k8sClient, obj), "failed to delete object %s", obj.GetName())
+		}
+	}()
+
+	for _, info := range telemetryGenObjInfos {
+		k8stest.WaitForTelemetryGenToStart(t, k8sClient, info.Namespace, info.PodLabelSelectors, info.Workload, info.DataType)
+	}
+
+	wantEntries := 128 // Minimal number of metrics/traces/logs to wait for.
+	waitForData(t, wantEntries, metricsConsumer, tracesConsumer, logsConsumer)
+
+	tcs := []struct {
+		name     string
+		dataType component.DataType
+		service  string
+		attrs    map[string]*expectedValue
+	}{
+		{
+			name:     "traces-job",
+			dataType: component.DataTypeTraces,
+			service:  "test-traces-job",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":       newExpectedValue(regex, "telemetrygen-"+testID+"-traces-job-[a-z0-9]*"),
+				"k8s.pod.uid":        newExpectedValue(regex, uidRe),
+				"k8s.job.name":       newExpectedValue(equal, "telemetrygen-"+testID+"-traces-job"),
+				"k8s.namespace.name": newExpectedValue(equal, testNs),
+				"k8s.node.name":      newExpectedValue(exist, ""),
+				"k8s.cluster.uid":    newExpectedValue(regex, uidRe),
+			},
+		},
+		{
+			name:     "traces-statefulset",
+			dataType: component.DataTypeTraces,
+			service:  "test-traces-statefulset",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":         newExpectedValue(equal, "telemetrygen-"+testID+"-traces-statefulset-0"),
+				"k8s.pod.uid":          newExpectedValue(regex, uidRe),
+				"k8s.statefulset.name": newExpectedValue(equal, "telemetrygen-"+testID+"-traces-statefulset"),
+				"k8s.namespace.name":   newExpectedValue(equal, testNs),
+				"k8s.node.name":        newExpectedValue(exist, ""),
+				"k8s.cluster.uid":      newExpectedValue(regex, uidRe),
+			},
+		},
+		{
+			name:     "traces-deployment",
+			dataType: component.DataTypeTraces,
+			service:  "test-traces-deployment",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":        newExpectedValue(regex, "telemetrygen-"+testID+"-traces-deployment-[a-z0-9]*-[a-z0-9]*"),
+				"k8s.pod.uid":         newExpectedValue(regex, uidRe),
+				"k8s.deployment.name": newExpectedValue(equal, "telemetrygen-"+testID+"-traces-deployment"),
+				"k8s.namespace.name":  newExpectedValue(equal, testNs),
+				"k8s.node.name":       newExpectedValue(exist, ""),
+				"k8s.cluster.uid":     newExpectedValue(regex, uidRe),
+			},
+		},
+		{
+			name:     "traces-daemonset",
+			dataType: component.DataTypeTraces,
+			service:  "test-traces-daemonset",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":       newExpectedValue(regex, "telemetrygen-"+testID+"-traces-daemonset-[a-z0-9]*"),
+				"k8s.pod.uid":        newExpectedValue(regex, uidRe),
+				"k8s.daemonset.name": newExpectedValue(equal, "telemetrygen-"+testID+"-traces-daemonset"),
+				"k8s.namespace.name": newExpectedValue(equal, testNs),
+				"k8s.node.name":      newExpectedValue(exist, ""),
+				"k8s.cluster.uid":    newExpectedValue(regex, uidRe),
+			},
+		},
+		{
+			name:     "metrics-job",
+			dataType: component.DataTypeMetrics,
+			service:  "test-metrics-job",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":       newExpectedValue(regex, "telemetrygen-"+testID+"-metrics-job-[a-z0-9]*"),
+				"k8s.pod.uid":        newExpectedValue(regex, uidRe),
+				"k8s.job.name":       newExpectedValue(equal, "telemetrygen-"+testID+"-metrics-job"),
+				"k8s.namespace.name": newExpectedValue(equal, testNs),
+				"k8s.node.name":      newExpectedValue(exist, ""),
+				"k8s.cluster.uid":    newExpectedValue(regex, uidRe),
+			},
+		},
+		{
+			name:     "metrics-statefulset",
+			dataType: component.DataTypeMetrics,
+			service:  "test-metrics-statefulset",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":         newExpectedValue(equal, "telemetrygen-"+testID+"-metrics-statefulset-0"),
+				"k8s.pod.uid":          newExpectedValue(regex, uidRe),
+				"k8s.statefulset.name": newExpectedValue(equal, "telemetrygen-"+testID+"-metrics-statefulset"),
+				"k8s.namespace.name":   newExpectedValue(equal, testNs),
+				"k8s.node.name":        newExpectedValue(exist, ""),
+				"k8s.cluster.uid":      newExpectedValue(regex, uidRe),
+			},
+		},
+		{
+			name:     "metrics-deployment",
+			dataType: component.DataTypeMetrics,
+			service:  "test-metrics-deployment",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":        newExpectedValue(regex, "telemetrygen-"+testID+"-metrics-deployment-[a-z0-9]*-[a-z0-9]*"),
+				"k8s.pod.uid":         newExpectedValue(regex, uidRe),
+				"k8s.deployment.name": newExpectedValue(equal, "telemetrygen-"+testID+"-metrics-deployment"),
+				"k8s.namespace.name":  newExpectedValue(equal, testNs),
+				"k8s.node.name":       newExpectedValue(exist, ""),
+				"k8s.cluster.uid":     newExpectedValue(regex, uidRe),
+			},
+		},
+		{
+			name:     "metrics-daemonset",
+			dataType: component.DataTypeMetrics,
+			service:  "test-metrics-daemonset",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":       newExpectedValue(regex, "telemetrygen-"+testID+"-metrics-daemonset-[a-z0-9]*"),
+				"k8s.pod.uid":        newExpectedValue(regex, uidRe),
+				"k8s.daemonset.name": newExpectedValue(equal, "telemetrygen-"+testID+"-metrics-daemonset"),
+				"k8s.namespace.name": newExpectedValue(equal, testNs),
+				"k8s.node.name":      newExpectedValue(exist, ""),
+				"k8s.cluster.uid":    newExpectedValue(regex, uidRe),
+			},
+		},
+		{
+			name:     "logs-job",
+			dataType: component.DataTypeLogs,
+			service:  "test-logs-job",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":       newExpectedValue(regex, "telemetrygen-"+testID+"-logs-job-[a-z0-9]*"),
+				"k8s.pod.uid":        newExpectedValue(regex, uidRe),
+				"k8s.job.name":       newExpectedValue(equal, "telemetrygen-"+testID+"-logs-job"),
+				"k8s.namespace.name": newExpectedValue(equal, testNs),
+				"k8s.node.name":      newExpectedValue(exist, ""),
+				"k8s.cluster.uid":    newExpectedValue(regex, uidRe),
+			},
+		},
+		{
+			name:     "logs-statefulset",
+			dataType: component.DataTypeLogs,
+			service:  "test-logs-statefulset",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":         newExpectedValue(equal, "telemetrygen-"+testID+"-logs-statefulset-0"),
+				"k8s.pod.uid":          newExpectedValue(regex, uidRe),
+				"k8s.statefulset.name": newExpectedValue(equal, "telemetrygen-"+testID+"-logs-statefulset"),
+				"k8s.namespace.name":   newExpectedValue(equal, testNs),
+				"k8s.node.name":        newExpectedValue(exist, ""),
+				"k8s.cluster.uid":      newExpectedValue(regex, uidRe),
+			},
+		},
+		{
+			name:     "logs-deployment",
+			dataType: component.DataTypeLogs,
+			service:  "test-logs-deployment",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":        newExpectedValue(regex, "telemetrygen-"+testID+"-logs-deployment-[a-z0-9]*-[a-z0-9]*"),
+				"k8s.pod.uid":         newExpectedValue(regex, uidRe),
+				"k8s.deployment.name": newExpectedValue(equal, "telemetrygen-"+testID+"-logs-deployment"),
+				"k8s.namespace.name":  newExpectedValue(equal, testNs),
+				"k8s.node.name":       newExpectedValue(exist, ""),
+				"k8s.cluster.uid":     newExpectedValue(regex, uidRe),
+			},
+		},
+		{
+			name:     "logs-daemonset",
+			dataType: component.DataTypeLogs,
+			service:  "test-logs-daemonset",
+			attrs: map[string]*expectedValue{
+				"k8s.pod.name":       newExpectedValue(regex, "telemetrygen-"+testID+"-logs-daemonset-[a-z0-9]*"),
+				"k8s.pod.uid":        newExpectedValue(regex, uidRe),
+				"k8s.daemonset.name": newExpectedValue(equal, "telemetrygen-"+testID+"-logs-daemonset"),
+				"k8s.namespace.name": newExpectedValue(equal, testNs),
+				"k8s.node.name":      newExpectedValue(exist, ""),
+				"k8s.cluster.uid":    newExpectedValue(regex, uidRe),
+			},
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			switch tc.dataType {
+			case component.DataTypeTraces:
+				scanTracesForAttributes(t, tracesConsumer, tc.service, tc.attrs)
+			case component.DataTypeMetrics:
+				scanMetricsForAttributes(t, metricsConsumer, tc.service, tc.attrs)
+			case component.DataTypeLogs:
+				scanLogsForAttributes(t, logsConsumer, tc.service, tc.attrs)
+			default:
+				t.Fatalf("unknown data type %s", tc.dataType)
+			}
+		})
+	}
+}
+
+func scanTracesForAttributes(t *testing.T, ts *consumertest.TracesSink, expectedService string,
+	kvs map[string]*expectedValue) {
+	// Iterate over the received set of traces starting from the most recent entries due to a bug in the processor:
+	// https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/18892
+	// TODO: Remove the reverse loop once it's fixed. All the metrics should be properly annotated.
+	for i := len(ts.AllTraces()) - 1; i >= 0; i-- {
+		traces := ts.AllTraces()[i]
+		for i := 0; i < traces.ResourceSpans().Len(); i++ {
+			resource := traces.ResourceSpans().At(i).Resource()
+			service, exist := resource.Attributes().Get("service.name")
+			assert.Equal(t, true, exist, "span do not has 'service.name' attribute in resource")
+			if service.AsString() != expectedService {
+				continue
+			}
+			assert.NoError(t, resourceHasAttributes(resource, kvs))
+			return
+		}
+	}
+	t.Fatalf("no spans found for service %s", expectedService)
+}
+
+func scanMetricsForAttributes(t *testing.T, ms *consumertest.MetricsSink, expectedService string,
+	kvs map[string]*expectedValue) {
+	// Iterate over the received set of metrics starting from the most recent entries due to a bug in the processor:
+	// https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/18892
+	// TODO: Remove the reverse loop once it's fixed. All the metrics should be properly annotated.
+	for i := len(ms.AllMetrics()) - 1; i >= 0; i-- {
+		metrics := ms.AllMetrics()[i]
+		for i := 0; i < metrics.ResourceMetrics().Len(); i++ {
+			resource := metrics.ResourceMetrics().At(i).Resource()
+			service, exist := resource.Attributes().Get("service.name")
+			assert.Equal(t, true, exist, "metric do not has 'service.name' attribute in resource")
+			if service.AsString() != expectedService {
+				continue
+			}
+			assert.NoError(t, resourceHasAttributes(resource, kvs))
+			return
+		}
+	}
+	t.Fatalf("no metric found for service %s", expectedService)
+}
+
+func scanLogsForAttributes(t *testing.T, ls *consumertest.LogsSink, expectedService string,
+	kvs map[string]*expectedValue) {
+	// Iterate over the received set of logs starting from the most recent entries due to a bug in the processor:
+	// https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/18892
+	// TODO: Remove the reverse loop once it's fixed. All the metrics should be properly annotated.
+	for i := len(ls.AllLogs()) - 1; i >= 0; i-- {
+		logs := ls.AllLogs()[i]
+		for i := 0; i < logs.ResourceLogs().Len(); i++ {
+			resource := logs.ResourceLogs().At(i).Resource()
+			service, exist := resource.Attributes().Get("service.name")
+			assert.Equal(t, true, exist, "log do not has 'service.name' attribute in resource")
+			if service.AsString() != expectedService {
+				continue
+			}
+			assert.NoError(t, resourceHasAttributes(resource, kvs))
+			return
+		}
+	}
+	t.Fatalf("no logs found for service %s", expectedService)
+}
+
+func resourceHasAttributes(resource pcommon.Resource, kvs map[string]*expectedValue) error {
+	foundAttrs := make(map[string]bool)
+	for k := range kvs {
+		foundAttrs[k] = false
+	}
+
+	resource.Attributes().Range(
+		func(k string, v pcommon.Value) bool {
+			if val, ok := kvs[k]; ok {
+				switch val.mode {
+				case equal:
+					if val.value == v.AsString() {
+						foundAttrs[k] = true
+					}
+				case regex:
+					matched, _ := regexp.MatchString(val.value, v.AsString())
+					if matched {
+						foundAttrs[k] = true
+					}
+				case exist:
+					foundAttrs[k] = true
+				}
+
+			}
+			return true
+		},
+	)
+
+	var err error
+	for k, v := range foundAttrs {
+		if !v {
+			err = multierr.Append(err, fmt.Errorf("%v attribute not found", k))
+		}
+	}
+	return err
+}
+
+func startUpSinks(t *testing.T, mc *consumertest.MetricsSink, tc *consumertest.TracesSink, lc *consumertest.LogsSink) func() {
+	f := otlpreceiver.NewFactory()
+	cfg := f.CreateDefaultConfig().(*otlpreceiver.Config)
+
+	_, err := f.CreateMetricsReceiver(context.Background(), receivertest.NewNopCreateSettings(), cfg, mc)
+	require.NoError(t, err, "failed creating metrics receiver")
+	_, err = f.CreateTracesReceiver(context.Background(), receivertest.NewNopCreateSettings(), cfg, tc)
+	require.NoError(t, err, "failed creating traces receiver")
+	rcvr, err := f.CreateLogsReceiver(context.Background(), receivertest.NewNopCreateSettings(), cfg, lc)
+	require.NoError(t, err, "failed creating logs receiver")
+	require.NoError(t, rcvr.Start(context.Background(), componenttest.NewNopHost()))
+	return func() {
+		assert.NoError(t, rcvr.Shutdown(context.Background()))
+	}
+}
+
+func waitForData(t *testing.T, entriesNum int, mc *consumertest.MetricsSink, tc *consumertest.TracesSink, lc *consumertest.LogsSink) {
+	timeoutMinutes := 3
+	require.Eventuallyf(t, func() bool {
+		return len(mc.AllMetrics()) > entriesNum && len(tc.AllTraces()) > entriesNum && len(lc.AllLogs()) > entriesNum
+	}, time.Duration(timeoutMinutes)*time.Minute, 1*time.Second,
+		"failed to receive %d entries,  received %d metrics, %d traces, %d logs in %d minutes", entriesNum,
+		len(mc.AllMetrics()), len(tc.AllTraces()), len(lc.AllLogs()), timeoutMinutes)
+}

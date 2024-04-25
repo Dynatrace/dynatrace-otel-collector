007bb0775bd24e66e90ab325728d248a60acc4d6
Joao Grassi <5938087+joaopgrassi@users.noreply.github.com>
2024-04-25 15:24:27 +0200

    Fix kubeconfig

--- testbed/integration/k8senrichment/e2e_test.go
+++ testbed/integration/k8senrichment/e2e_test.go
@@ -28,7 +28,7 @@ const (
 	equal = iota
 	regex
 	exist
-	testKubeConfig = "/home/grassi/.kube/config"
+	testKubeConfig = "/tmp/kube-config-collector-e2e-testing"
 	uidRe          = "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}"
 )
 

e69c08d8aa6f78163ce74b65336d8f7f0cbfe175
github-action-benchmark <github@users.noreply.github.com>
2024-09-10 08:56:13 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 990e3b0229a43127192b38847d300983aee24ade

--- docs/benchmarks/loadtests/data.js
+++ docs/benchmarks/loadtests/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725884408208,
+  "lastUpdate": 1725958573817,
   "repoUrl": "https://github.com/Dynatrace/dynatrace-otel-collector",
   "entries": {
     "Benchmark": [
@@ -42,6 +42,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "ram_mib_max",
             "value": 94,
+            "unit": "MiB",
+            "extra": "Log10kDPS/OTLP - RAM (MiB)"
+          },
+          {
+            "name": "dropped_span_count",
+            "value": 0,
+            "unit": "spans",
+            "extra": "Log10kDPS/OTLP - Dropped Span Count"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "93584209+odubajDT@users.noreply.github.com",
+            "name": "odubajDT",
+            "username": "odubajDT"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "990e3b0229a43127192b38847d300983aee24ade",
+          "message": "[chore]: add load test workflow (#283)\n\nSigned-off-by: odubajDT <ondrej.dubaj@dynatrace.com>\r\nCo-authored-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>",
+          "timestamp": "2024-09-10T08:49:13Z",
+          "tree_id": "1683cfb81084440896b92d207e98ca04649f63b2",
+          "url": "https://github.com/Dynatrace/dynatrace-otel-collector/commit/990e3b0229a43127192b38847d300983aee24ade"
+        },
+        "date": 1725958573001,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "cpu_percentage_avg",
+            "value": 10.26536584977155,
+            "unit": "%",
+            "extra": "Log10kDPS/OTLP - Cpu Percentage"
+          },
+          {
+            "name": "cpu_percentage_max",
+            "value": 10.99792986699309,
+            "unit": "%",
+            "extra": "Log10kDPS/OTLP - Cpu Percentage"
+          },
+          {
+            "name": "ram_mib_avg",
+            "value": 67,
+            "unit": "MiB",
+            "extra": "Log10kDPS/OTLP - RAM (MiB)"
+          },
+          {
+            "name": "ram_mib_max",
+            "value": 95,
             "unit": "MiB",
             "extra": "Log10kDPS/OTLP - RAM (MiB)"
           },

7bc1373cde8245682105fe98cf7ba41dc23fa529
Moritz Wiesinger <moritz.wiesinger@dynatrace.com>
2024-09-10 11:23:16 +0200

    add metrics load tests first version
    Signed-off-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>

---
+++ internal/testbed/load/tests/metrics_test.go
@@ -0,0 +1,61 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package loadtest
+
+// This file contains Test functions which initiate the tests. The tests can be either
+// coded in this file or use scenarios from perf_scenarios.go.
+
+import (
+	"testing"
+
+	"github.com/Dynatrace/dynatrace-otel-collector/internal/testcommon/testutil"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/testbed/testbed"
+)
+
+func TestMetric10kDPS(t *testing.T) {
+	tests := []struct {
+		name         string
+		sender       testbed.DataSender
+		receiver     testbed.DataReceiver
+		resourceSpec testbed.ResourceSpec
+		skipMessage  string
+	}{
+		{
+			name:     "OTLP",
+			sender:   testbed.NewOTLPMetricDataSender(testbed.DefaultHost, testutil.GetAvailablePort(t)),
+			receiver: testbed.NewOTLPDataReceiver(testutil.GetAvailablePort(t)),
+			resourceSpec: testbed.ResourceSpec{
+				ExpectedMaxCPU: 60,
+				ExpectedMaxRAM: 105,
+			},
+		},
+		{
+			name:     "OTLP-HTTP",
+			sender:   testbed.NewOTLPHTTPMetricDataSender(testbed.DefaultHost, testutil.GetAvailablePort(t)),
+			receiver: testbed.NewOTLPHTTPDataReceiver(testutil.GetAvailablePort(t)),
+			resourceSpec: testbed.ResourceSpec{
+				ExpectedMaxCPU: 60,
+				ExpectedMaxRAM: 100,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if test.skipMessage != "" {
+				t.Skip(test.skipMessage)
+			}
+			Scenario10kItemsPerSecond(
+				t,
+				test.sender,
+				test.receiver,
+				test.resourceSpec,
+				performanceResultsSummary,
+				nil,
+				nil,
+			)
+		})
+	}
+
+}

66a6419bc215a39000a4be50db0e4630e87b31a6
github-action-benchmark <github@users.noreply.github.com>
2024-09-10 09:18:02 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 7008c5295a5f3ac9f729eda34888041d068c7610

--- docs/benchmarks/loadtests/data.js
+++ docs/benchmarks/loadtests/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725958573817,
+  "lastUpdate": 1725959882331,
   "repoUrl": "https://github.com/Dynatrace/dynatrace-otel-collector",
   "entries": {
     "Benchmark": [
@@ -90,6 +90,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "ram_mib_avg",
             "value": 67,
+            "unit": "MiB",
+            "extra": "Log10kDPS/OTLP - RAM (MiB)"
+          },
+          {
+            "name": "ram_mib_max",
+            "value": 95,
+            "unit": "MiB",
+            "extra": "Log10kDPS/OTLP - RAM (MiB)"
+          },
+          {
+            "name": "dropped_span_count",
+            "value": 0,
+            "unit": "spans",
+            "extra": "Log10kDPS/OTLP - Dropped Span Count"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7008c5295a5f3ac9f729eda34888041d068c7610",
+          "message": "chore(deps): update opentelemetry collector to v1 (major) (#287)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>\r\nCo-authored-by: Moritz Wiesinger <moritz.wiesinger@dynatrace.com>",
+          "timestamp": "2024-09-10T09:11:42Z",
+          "tree_id": "143f393badd9ca0561470a4b226404ad120afa5a",
+          "url": "https://github.com/Dynatrace/dynatrace-otel-collector/commit/7008c5295a5f3ac9f729eda34888041d068c7610"
+        },
+        "date": 1725959881441,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "cpu_percentage_avg",
+            "value": 10.665810650746515,
+            "unit": "%",
+            "extra": "Log10kDPS/OTLP - Cpu Percentage"
+          },
+          {
+            "name": "cpu_percentage_max",
+            "value": 11.329491672649514,
+            "unit": "%",
+            "extra": "Log10kDPS/OTLP - Cpu Percentage"
+          },
+          {
+            "name": "ram_mib_avg",
+            "value": 66,
             "unit": "MiB",
             "extra": "Log10kDPS/OTLP - RAM (MiB)"
           },

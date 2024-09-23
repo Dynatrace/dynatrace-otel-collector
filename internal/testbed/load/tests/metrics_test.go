de8b0a812a499d2546d4c0bd258ab9142120f33e
odubajDT <ondrej.dubaj@dynatrace.com>
2024-09-23 08:49:01 +0200

    bump metrics test resources
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- internal/testbed/load/tests/metrics_test.go
+++ internal/testbed/load/tests/metrics_test.go
@@ -118,7 +118,7 @@ func TestMetric100kDPS(t *testing.T) {
 				},
 				resourceSpec: testbed.ResourceSpec{
 					ExpectedMaxCPU: 70,
-					ExpectedMaxRAM: 105,
+					ExpectedMaxRAM: 120,
 				},
 				attrCount:       25,
 				attrSizeByte:    20,

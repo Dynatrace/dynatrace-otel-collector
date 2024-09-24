b65cc9d80b4bd57a3bb30c3e5ed9007de47bfb90
Florian Bacher <florian.bacher@dynatrace.com>
2024-09-24 15:53:04 +0200

    additional scenario
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- internal/testbed/load/tests/metrics_test.go
+++ internal/testbed/load/tests/metrics_test.go
@@ -223,6 +223,30 @@ func TestPrometheusMetric(t *testing.T) {
 			processors: metricProcessors,
 		},
 		{
+			name:     "Prometheus Prometheus 1kDPS - 5 Prometheus Endpoints",
+			sender:   datasenders2.NewMultiHostPrometheusDataSender(testbed.DefaultHost, getAvailablePorts(t, 5)),
+			receiver: testbed.NewOTLPDataReceiver(testutil.GetAvailablePort(t)),
+			extendedLoadOptions: ExtendedLoadOptions{
+				resourceSpec: testbed.ResourceSpec{
+					ExpectedMaxCPU: 150,
+					ExpectedMaxRAM: 1500,
+				},
+				loadOptions: &testbed.LoadOptions{
+					DataItemsPerSecond: 1,
+					ItemsPerBatch:      1,
+					Parallel:           1,
+				},
+				attrCount:       25,
+				attrSizeByte:    20,
+				attrKeySizeByte: 100,
+				scrapeLoadOptions: scrapeLoadOptions{
+					numberOfMetrics:            1000,
+					scrapeIntervalMilliSeconds: 1000,
+				},
+			},
+			processors: metricProcessors,
+		},
+		{
 			name:     "Prometheus Prometheus 1kDPS - 10 Prometheus Endpoints",
 			sender:   datasenders2.NewMultiHostPrometheusDataSender(testbed.DefaultHost, getAvailablePorts(t, 10)),
 			receiver: testbed.NewOTLPDataReceiver(testutil.GetAvailablePort(t)),

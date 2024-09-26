34492940b874cf4bb6c5d42e89ae141e62e23457
odubajDT <ondrej.dubaj@dynatrace.com>
2024-09-26 14:58:54 +0200

    [chore] remove duplicated versioning from oteltestbedcol
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- internal/testbed/load/tests/metrics_test.go
+++ internal/testbed/load/tests/metrics_test.go
@@ -72,7 +72,7 @@ func TestMetric10kDPS(t *testing.T) {
 				},
 				resourceSpec: testbed.ResourceSpec{
 					ExpectedMaxCPU: 60,
-					ExpectedMaxRAM: 105,
+					ExpectedMaxRAM: 120,
 				},
 				attrCount:       25,
 				attrSizeByte:    20,
@@ -118,7 +118,7 @@ func TestMetric100kDPS(t *testing.T) {
 				},
 				resourceSpec: testbed.ResourceSpec{
 					ExpectedMaxCPU: 70,
-					ExpectedMaxRAM: 105,
+					ExpectedMaxRAM: 120,
 				},
 				attrCount:       25,
 				attrSizeByte:    20,
@@ -137,8 +137,8 @@ func TestMetric100kDPS(t *testing.T) {
 					Parallel:           1,
 				},
 				resourceSpec: testbed.ResourceSpec{
-					ExpectedMaxCPU: 99,
-					ExpectedMaxRAM: 100,
+					ExpectedMaxCPU: 100,
+					ExpectedMaxRAM: 120,
 				},
 				attrCount:       25,
 				attrSizeByte:    20,

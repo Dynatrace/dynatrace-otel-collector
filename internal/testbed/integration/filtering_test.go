60d2089435b7e261ea554f462219819be6b8027d
Florian Bacher <florian.bacher@dynatrace.com>
2024-10-23 09:30:14 +0200

    add test for filtering using the transform processor
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- internal/testbed/integration/filtering_test.go
+++ internal/testbed/integration/filtering_test.go
@@ -65,7 +65,7 @@ func TestFiltering(t *testing.T) {
 	}
 }
 
-func TestFilteringDTAPIToken(t *testing.T) {
+func TestFilteringDTAPITokenRedactionProcessor(t *testing.T) {
 	redactionProcessor := `
   redaction:
     allow_all_keys: true
@@ -127,6 +127,99 @@ func TestFilteringDTAPIToken(t *testing.T) {
 			validator:    NewSyslogSampleConfigValidator(t, expectedLog),
 			processors: map[string]string{
 				"redaction": redactionProcessor,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			FilteringScenario(
+				t,
+				test.dataProvider,
+				test.sender,
+				test.receiver,
+				test.validator,
+				test.processors,
+				nil,
+			)
+		})
+	}
+}
+
+func TestFilteringDTAPITokenTransformProcessor(t *testing.T) {
+	transformProcessor := `
+  transform:
+    trace_statements:
+      - context: span
+        statements:
+          - replace_all_patterns(attributes, "value", "(dt0s0[1-9].[A-Za-z0-9]{24}.)([A-Za-z0-9]{64})", "$1****")
+    metric_statements:
+      - context: datapoint
+        statements:
+          - replace_all_patterns(attributes, "value", "(dt0s0[1-9].[A-Za-z0-9]{24}.)([A-Za-z0-9]{64})", "$1****")
+      - context: resource
+        statements:
+          - replace_all_patterns(attributes, "value", "(dt0s0[1-9].[A-Za-z0-9]{24}.)([A-Za-z0-9]{64})", "$1****")
+    log_statements:
+      - context: log
+        statements:
+          - replace_all_patterns(attributes, "value", "(dt0s0[1-9].[A-Za-z0-9]{24}.)([A-Za-z0-9]{64})", "$1****")
+`
+	ingestedAttrs := map[string]string{
+		// NOTE: the sample token below is NOT an actual token, but an example taken from the DT docs: https://docs.dynatrace.com/docs/dynatrace-api/basics/dynatrace-api-authentication
+		"t": "dt0s01.ST2EY72KQINMH574WMNVI7YN.G3DFPBEJYMODIDAEX454M7YWBUVEFOWKPRVMWFASS64NFH52PX6BNDVFFM572RZM",
+	}
+
+	expectedAttrs := map[string]string{
+		"t": "dt0s01.ST2EY72KQINMH574WMNVI7YN.****",
+	}
+
+	ingestedTrace := generateBasicTrace(ingestedAttrs)
+	expectedTrace := generateBasicTrace(expectedAttrs)
+
+	ingestedMetric := generateBasicMetric(ingestedAttrs)
+	expectedMetric := generateBasicMetric(expectedAttrs)
+
+	now := pcommon.NewTimestampFromTime(time.Now())
+	ingestedLog := generateBasicLogs(now, ingestedAttrs)
+	expectedLog := generateBasicLogs(now, expectedAttrs)
+
+	tests := []struct {
+		name         string
+		dataProvider testbed.DataProvider
+		sender       testbed.DataSender
+		receiver     testbed.DataReceiver
+		validator    testbed.TestCaseValidator
+		processors   map[string]string
+	}{
+		{
+			name:         "basic traces",
+			dataProvider: NewSampleConfigsTraceDataProvider(ingestedTrace),
+			sender:       testbed.NewOTLPTraceDataSender(testbed.DefaultHost, testutil.GetAvailablePort(t)),
+			receiver:     testbed.NewOTLPDataReceiver(testutil.GetAvailablePort(t)),
+			validator:    NewTraceSampleConfigsValidator(t, expectedTrace, WithTraceAttributeCheck()),
+			processors: map[string]string{
+				"transform": transformProcessor,
+			},
+		},
+		{
+			name:         "basic metrics",
+			dataProvider: NewSampleConfigsMetricsDataProvider(ingestedMetric),
+			sender:       testbed.NewOTLPMetricDataSender(testbed.DefaultHost, testutil.GetAvailablePort(t)),
+			receiver:     testbed.NewOTLPDataReceiver(testutil.GetAvailablePort(t)),
+			validator:    NewMetricSampleConfigsValidator(t, expectedMetric),
+			processors: map[string]string{
+				"transform": transformProcessor,
+			},
+		},
+		{
+			name:         "basic logs",
+			dataProvider: NewSampleConfigsLogsDataProvider(ingestedLog),
+			sender:       testbed.NewOTLPLogsDataSender(testbed.DefaultHost, testutil.GetAvailablePort(t)),
+			receiver:     testbed.NewOTLPDataReceiver(testutil.GetAvailablePort(t)),
+			validator:    NewSyslogSampleConfigValidator(t, expectedLog),
+			processors: map[string]string{
+				"transform": transformProcessor,
 			},
 		},
 	}

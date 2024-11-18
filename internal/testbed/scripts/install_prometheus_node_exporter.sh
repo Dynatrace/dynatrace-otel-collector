9506b5561cdedfa9aa12454a404145b5baf73f84
odubajDT <93584209+odubajDT@users.noreply.github.com>
2024-11-18 14:32:00 +0100

    [chore] cleanup and refactor testing workflows (#373)
    Signed-off-by: odubajDT <ondrej.dubaj@dynatrace.com>

--- internal/testbed/scripts/install_prometheus_node_exporter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-kubectl create namespace prometheus-node-exporter
-helm install prometheus-node-exporter prometheus-community/prometheus-node-exporter --namespace prometheus-node-exporter
-
-# Wait until the node exporter is up and running
-kubectl rollout --timeout 120s status daemonset/prometheus-node-exporter -n prometheus-node-exporter

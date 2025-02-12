c976730a5308c1ebf4150268e35ef11d57c709f3
Florian Bacher <florian.bacher@dynatrace.com>
2025-02-12 11:25:23 +0100

    [chore] add fluent sender to data ingest cli (#457)
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- internal/data-ingest-cli/README.md
+++ internal/data-ingest-cli/README.md
@@ -16,6 +16,7 @@ making it easier to diagnose and resolve problems related to OTLP data handling.
   - OTLP Json
   - Statsd
   - Syslog
+  - Fluent
 
 - Receive OTLP data via an endpoint: The tool can receive OTLP data through an endpoint and store the received payload in a specified file in OTLP Json format.
 
@@ -36,7 +37,7 @@ go build -o data-ingest
 The tool accepts the following input parameters:
 
 - `--input-file`: The name of the input file to read data from.
-- `--input-format`: The input format of the ingested data (options: `otlp-json`, `syslog`, `statsd`).
+- `--input-format`: The input format of the ingested data (options: `otlp-json`, `syslog`, `statsd`, `fluent`).
 - `--collector-url`: The URL of the OpenTelemetry collector.
 - `--output-file`: The file in which to store the received data.
 - `--receiver-port`: The port of the OTLP receiver created to act as a sink for the collector.
@@ -62,4 +63,10 @@ The tool accepts the following input parameters:
 
 ```shell
 ./data-ingest --input-format syslog --input-file $(pwd)/commands/syslog/testdata/rfc6587-non-transparent-framing --collector-url localhost:54526
+```
+
+1. Send Fluent data to a collector:
+
+```shell
+./data-ingest --input-format fluent --input-file $(pwd)/commands/fluent/testdata/msg.json --collector-url localhost:8006
 ```
---
+++ internal/data-ingest-cli/commands/fluent/command.go
@@ -0,0 +1,105 @@
+package fluent
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/Dynatrace/dynatrace-otel-collector/internal/data-ingest-cli/receiver"
+	otlpreceiver "github.com/Dynatrace/dynatrace-otel-collector/internal/data-ingest-cli/receiver/otlp"
+	"github.com/Dynatrace/dynatrace-otel-collector/internal/data-ingest-cli/receiver/otlphttp"
+	"github.com/Dynatrace/dynatrace-otel-collector/internal/data-ingest-cli/sender/fluent"
+	"net/url"
+	"os"
+	"strconv"
+)
+
+var errMissingFluentProperties = fmt.Errorf("test data must be a json object containing a 'tag' and 'message' property")
+
+type Config struct {
+	InputFile    string
+	CollectorURL string
+	Transport    string
+	OutputFile   string
+	ReceiverPort int
+	ReceiverType string
+}
+
+type Cmd struct {
+	cfg      Config
+	receiver receiver.Receiver
+	sender   fluent.Sender
+}
+
+func New(cfg Config) (*Cmd, error) {
+	c := &Cmd{
+		cfg: cfg,
+	}
+
+	if cfg.ReceiverPort > 0 && cfg.OutputFile != "" {
+		switch cfg.ReceiverType {
+		case "grpc":
+			c.receiver = otlpreceiver.NewOTLPReceiver(otlpreceiver.Config{
+				Port:       cfg.ReceiverPort,
+				OutputFile: cfg.OutputFile,
+			})
+		case "http":
+			c.receiver = otlphttp.NewOTLPHTTPReceiver(otlphttp.Config{
+				Port:       cfg.ReceiverPort,
+				OutputFile: cfg.OutputFile,
+			})
+		default:
+			return nil, fmt.Errorf("invalid receiver type %s", cfg.ReceiverType)
+		}
+	}
+
+	collectorURL, err := url.Parse(cfg.CollectorURL)
+	if err != nil {
+		return nil, err
+	}
+	port, err := strconv.Atoi(collectorURL.Port())
+	if err != nil {
+		return nil, err
+	}
+	sender, err := fluent.New(collectorURL.Hostname(), port)
+	if err != nil {
+		return nil, err
+	}
+	c.sender = sender
+
+	return c, nil
+}
+
+func (c *Cmd) Do(_ context.Context) error {
+	if c.receiver != nil {
+		if err := c.receiver.Start(); err != nil {
+			return err
+		}
+		defer c.receiver.Stop()
+	}
+	return c.sendLogs()
+}
+
+func (c *Cmd) sendLogs() error {
+	fileContent, err := os.ReadFile(c.cfg.InputFile)
+	if err != nil {
+		return err
+	}
+
+	content := map[string]any{}
+
+	if err := json.Unmarshal(fileContent, &content); err != nil {
+		return err
+	}
+
+	tag, ok := content["tag"].(string)
+	if !ok {
+		return errMissingFluentProperties
+	}
+
+	msg, ok := content["message"].(map[string]interface{})
+	if !ok {
+		return errMissingFluentProperties
+	}
+
+	return c.sender.Write(tag, msg)
+}

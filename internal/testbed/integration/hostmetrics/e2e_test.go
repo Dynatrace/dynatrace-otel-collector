f484bd1b1ed753ec9a3eccc5b90f4a65cc95b7d6
RealAnna <89971034+RealAnna@users.noreply.github.com>
2026-03-16 09:07:58 +0100

    Update internal/testbed/integration/hostmetrics/e2e_test.go
    Co-authored-by: odubajDT <93584209+odubajDT@users.noreply.github.com>

--- internal/testbed/integration/hostmetrics/e2e_test.go
+++ internal/testbed/integration/hostmetrics/e2e_test.go
@@ -324,6 +324,11 @@ func checkMetrics(t *testing.T, expectedFile string, consumer *consumertest.Metr
 		err := pmetrictest.CompareMetrics(expectedMerged, actualMerged, options...)
 		// the commented line below prints the diff between expected and actual metrics in case of a test failure
 		// testutil.Debug(err, t, expectedMerged, actualMerged)
+require.EventuallyWithT(t, func(tt *assert.CollectT) {
+        actualMerged := testutil.MergeResources(consumer.AllMetrics()[len(consumer.AllMetrics())-1])
+		err := pmetrictest.CompareMetrics(expectedMerged, actualMerged, options...)
+		// the commented line below prints the diff between expected and actual metrics in case of a test failure
+		// testutil.Debug(err, t, expectedMerged, actualMerged)
 		assert.NoError(tt, err)
 	}, timeout, tick)
 }

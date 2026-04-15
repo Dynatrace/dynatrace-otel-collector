c7bc9f2d0f72a8beedb181296d563a1f6aa5e054
RealAnna <anna.reale@dynatrace.com>
2026-04-15 11:48:24 +0200

    cange overlay

--- internal/testbed/integration/self-monitoring-prometheus/e2e_test.go
+++ internal/testbed/integration/self-monitoring-prometheus/e2e_test.go
@@ -132,10 +132,10 @@ func Test_Selfmonitoring_Prometheus_checkMetrics(t *testing.T) {
 	oteltest.WaitForMetrics(t, 1, telemetrygenMetricsConsumer)
 
 	// self monitoring metrics
-	oteltest.WaitForMetrics(t, 1, metricsConsumer)
+	oteltest.WaitForMetrics(t, 5, metricsConsumer)
 
 	// Uncomment to regenerate golden:
-	// require.NoError(t, golden.WriteMetrics(t, expectedFile, metricsConsumer.AllMetrics()[len(metricsConsumer.AllMetrics())-1]))
+	//require.NoError(t, golden.WriteMetrics(t, expectedFile, metricsConsumer.AllMetrics()[len(metricsConsumer.AllMetrics())-1]))
 
 	expected, err := golden.ReadMetrics(expectedFile)
 	require.NoError(t, err)
